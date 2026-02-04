4e8eb29dd41c3e45ab1518dffa8e48ae3f3b481a
Luan Nico <luanpotter27@gmail.com>
2026-02-04 02:23:04 -0500

    About

--- src/app/pages/about/About.tsx
+++ src/app/pages/about/About.tsx
@@ -10,6 +10,9 @@ export function About() {
         Check out the source code on <a href="https://github.com/luanpotter/card-press">GitHub</a>.
       </div>
       <div>Leave us a star or contribute if you found it useful!</div>
+      <hr />
+      <div>A fully client-side webapp for generating print-then-cut PDFs for playing cards.</div>
+      <div>Cricut instructions coming soon!</div>
     </section>
   );
 }
